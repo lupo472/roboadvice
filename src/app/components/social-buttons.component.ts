be9057e504a8c93f041cbfa796e0d680ec7b5247
harrier91 <andreamaga4@gmail.com>
2017-02-27 17:59:23 +0100

    frontend template

---
+++ src/app/components/social-buttons.component.ts
@@ -0,0 +1,74 @@
+import { Component }    from '@angular/core';
+
+@Component({
+  templateUrl: 'social-buttons.component.html'
+})
+export class SocialButtonsComponent {
+
+  constructor() { }
+  
+  // social box charts
+
+  public socialChartData1:Array<any> = [
+    {
+      data: [65, 59, 84, 84, 51, 55, 40],
+      label: 'Facebook'
+    }
+  ];
+  public socialChartData2:Array<any> = [
+    {
+      data: [1, 13, 9, 17, 34, 41, 38],
+      label: 'Twitter'
+    }
+  ];
+  public socialChartData3:Array<any> = [
+    {
+      data: [78, 81, 80, 45, 34, 12, 40],
+      label: 'LinkedIn'
+    }
+  ];
+  public socialChartData4:Array<any> = [
+    {
+      data: [35, 23, 56, 22, 97, 23, 64],
+      label: 'Google+'
+    }
+  ];
+
+  public socialChartLabels:Array<any> = ['January','February','March','April','May','June','July'];
+  public socialChartOptions:any = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      xAxes: [{
+        display:false,
+      }],
+      yAxes: [{
+        display:false,
+      }]
+    },
+    elements: {
+      line: {
+        borderWidth: 2
+      },
+      point: {
+        radius: 0,
+        hitRadius: 10,
+        hoverRadius: 4,
+        hoverBorderWidth: 3,
+      }
+    },
+    legend: {
+      display: false
+    }
+  };
+  public socialChartColours:Array<any> = [
+    {
+      backgroundColor: 'rgba(255,255,255,.1)',
+      borderColor: 'rgba(255,255,255,.55)',
+      pointHoverBackgroundColor: '#fff'
+    }
+  ];
+  public socialChartLegend:boolean = false;
+  public socialChartType:string = 'line';
+
+}
---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { DropdownModule } from 'ng2-bootstrap/dropdown';
+
+import { CommonModule } from '@angular/common';
+
+import { DashboardComponent } from './dashboard.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+
+@NgModule({
+  imports: [
+    DashboardRoutingModule,
+    ChartsModule,
+    DropdownModule,
+    CommonModule
+  ],
+  declarations: [ DashboardComponent ]
+})
+export class DashboardModule { }
