310116edb078bac879b897d7fe9c96930c79e151
harrier91 <andreamaga4@gmail.com>
2017-02-27 20:18:53 +0100

    linechart update dynamically

--- src/app/dashboard/card-asset-class/card-asset-class.component.ts
+++ src/app/dashboard/card-asset-class/card-asset-class.component.ts
@@ -7,8 +7,14 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardAssetClassComponent implements OnInit {
   
-  @Input() 
-    value;
+    @Input() value;
+    @Input() lineChartData;
+    @Input() lineChartLabels;
+    @Input() lineChartOptions;
+    @Input() lineChartColours;
+    @Input() lineChartLegend;
+    @Input() lineChartType;
+  
 
   constructor() {}
   
@@ -18,60 +24,6 @@ export class CardAssetClassComponent implements OnInit {
   public brandWarning:string =  '#f8cb00';
   public brandDanger:string =   '#f86c6b';
   
-  // lineChart1
-  public lineChart1Data:Array<any> = [
-    {
-      data: [65, 59, 84, 84, 51, 55, 40],
-      label: 'Series A'
-    }
-  ];
-  public lineChart1Labels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChart1Options:any = {
-    maintainAspectRatio: false,
-    scales: {
-      xAxes: [{
-        gridLines: {
-          color: 'transparent',
-          zeroLineColor: 'transparent'
-        },
-        ticks: {
-          fontSize: 2,
-          fontColor: 'transparent',
-        }
-
-      }],
-      yAxes: [{
-        display: false,
-        ticks: {
-          display: false,
-          min: 40 - 5,
-          max: 84 + 5,
-        }
-      }],
-    },
-    elements: {
-      line: {
-        borderWidth: 1
-      },
-      point: {
-        radius: 4,
-        hitRadius: 10,
-        hoverRadius: 4,
-      },
-    },
-    legend: {
-      display: false
-    }
-  };
-  public lineChart1Colours:Array<any> = [
-    { // grey
-      backgroundColor: this.brandPrimary,
-      borderColor: 'rgba(255,255,255,.55)'
-    }
-  ];
-  public lineChart1Legend:boolean = false;
-  public lineChart1Type:string = 'line';
-
   ngOnInit() {
   }
 
